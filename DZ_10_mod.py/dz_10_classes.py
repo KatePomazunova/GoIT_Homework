18a8a18ec3251d15a93d7b049c50aea29c90905d
KatePomazunova <kateryna.pomazunova@gmail.com>
2022-12-03 23:06:07 +0200

    DZ_10_mod

---
+++ DZ_10_mod.py/dz_10_classes.py
@@ -0,0 +1,73 @@
+from collections import UserDict
+
+
+
+class AddressBook(UserDict):
+    """Логіка пошуку за записами до цього класу."""
+    
+    def __init__(self):
+        self.data = {}
+        
+
+    def add_record(self, record):
+        self.data[record.phone_name.value] = record
+
+        
+    def change_record(self, record):
+        self.data.update({record.phone_name.value: record})
+
+
+    def find_contact(self, name):
+        for phone_name, numbers in self.data.items():
+            if phone_name == name:
+                return [number.value for number in numbers.phone_numbers]
+        return "Not found this contact"
+
+
+class Field:
+    """Є батьківським для всіх полів, у ньому реалізуємо логіку 
+    загальну для всіх полів."""
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    """Обов'язкове поле з ім'ям."""
+    pass
+
+
+class Phone(Field):
+    """Необов'язкове поле з телефоном."""
+    pass
+
+
+class Record:
+    """відповідає за логіку додавання/видалення/редагування необов'язкових полів
+    та зберігання обов'язкового поля Name."""
+    def __init__(self, name):
+
+        self.phone_name = Name(name)
+        self.phone_numbers = []
+    
+
+    def add(self, phone):
+        
+        self.phone_numbers.append(Phone(phone))
+
+
+    def change(self, phone):
+       
+        self.phone_numbers.append(Phone(phone))
+
+
+    def delete(self, phone):
+        
+        for item in self.phone_numbers:
+            if item == phone:
+                del item
+                return f"Number {Phone(phone)} deleted."
+        return "Not found phone number"    
+
+
+
+
